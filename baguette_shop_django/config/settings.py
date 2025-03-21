aec8ce39ada471d46b31c8f20ef77f15746558b0
Kirill Batrakov <dl9ceb9@gmail.com>
2025-03-22 00:35:24 +0700

    update SECRET_KEY

--- baguette_shop_django/config/settings.py
+++ baguette_shop_django/config/settings.py
@@ -12,17 +12,21 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os.path
 from os import environ
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(lk4qxob9j)19+fw$#vcwq#(!u551&w8+sivaxca^(m)1m=kfm'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
