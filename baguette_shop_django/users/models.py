a1408eb3911953d91e6e732ee64e0463b3dec43f
Kirill Batrakov <dl9ceb9@gmail.com>
2025-05-25 15:33:26 +0700

    I think finally models and views for USERS done at THIS point

--- baguette_shop_django/users/models.py
+++ baguette_shop_django/users/models.py
@@ -1,5 +1,4 @@
-import uuid
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
 
@@ -7,8 +6,14 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Email field must be set")
+
+        is_staff = extra_fields.pop('is_staff', False)
+        is_superuser = extra_fields.pop('is_superuser', False)
+
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
+        user.is_staff = is_staff
+        user.is_superuser = is_superuser
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -19,8 +24,7 @@ class UserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 
-class User(AbstractBaseUser):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
@@ -29,8 +33,16 @@ class User(AbstractBaseUser):
 
     objects = UserManager()
     USERNAME_FIELD = 'email'
+
+    # это для создания суперюзера
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
     # переопределен потому что по дефолту  возвращает username
     def __str__(self):
         return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
