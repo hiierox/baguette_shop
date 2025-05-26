d92b0e856fa60714d135cb497a75e581b911ead9
Kirill Batrakov <dl9ceb9@gmail.com>
2025-05-26 20:27:50 +0700

    api/docs fix

--- baguette_shop_django/config/settings.py
+++ baguette_shop_django/config/settings.py
@@ -50,7 +50,12 @@ INSTALLED_APPS = [
     'orders',
     'products',
     'users',
+    'drf_spectacular',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
