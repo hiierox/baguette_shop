f970e22de33539bb11c269df69841ebdf54b3153
Kirill Batrakov <dl9ceb9@gmail.com>
2025-06-03 19:14:29 +0700

    add admin panel for products

--- baguette_shop_django/products/admin.py
+++ baguette_shop_django/products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'material')
+    search_fields = ('name', 'material')
+    filter_horizontal = ('categories',)
+    fields = ('name', 'description', 'price', 'material', 'width', 'height', 'image', 'categories')
--- baguette_shop_django/products/models.py
+++ baguette_shop_django/products/models.py
@@ -9,14 +9,14 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    material = models.CharField(max_length=100)
-    width = models.FloatField()
-    height = models.FloatField()
-    image = models.ImageField(upload_to='products', blank=True)
-    categories = models.ManyToManyField(Category)
+    name = models.CharField(max_length=100, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Цена')
+    material = models.CharField(max_length=100, verbose_name='Материал')
+    width = models.FloatField(verbose_name='Ширина')
+    height = models.FloatField(verbose_name='Высота')
+    image = models.ImageField(upload_to='products', blank=True, verbose_name='Изображение')
+    categories = models.ManyToManyField(Category, verbose_name='Категории')
 
     def __str__(self):
         return self.name
